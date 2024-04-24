a46222a065e42adf4d5a61c344624550fdaac793
Markuss Puhovs <s22puhomark@vea.lv>
2024-04-24 10:31:03 +0300

    MyTreeNode

--- src/DataStr/MyTreeNode.java
+++ src/DataStr/MyTreeNode.java
@@ -1,5 +1,44 @@
 package DataStr;
 
-public class MyTreeNode {
-
+public class MyTreeNode<Ttype> {
+	private Ttype element;
+	private MyTreeNode parent = null;
+	private MyTreeNode rightCh = null;
+	private MyTreeNode leftCh = null;
+	
+	public Ttype getElement() {
+		return element;
+	}
+	public void setElement(Ttype element) {
+		if(element != null)
+			this.element = element;
+		else
+			this.element = (Ttype) new Object();
+	}
+	public MyTreeNode getParent() {
+		return parent;
+	}
+	public void setParent(MyTreeNode parent) {
+		this.parent = parent;
+	}
+	public MyTreeNode getRightCh() {
+		return rightCh;
+	}
+	public void setRightCh(MyTreeNode rightCh) {
+		this.rightCh = rightCh;
+	}
+	public MyTreeNode getLeftCh() {
+		return leftCh;
+	}
+	public void setLeftCh(MyTreeNode leftCh) {
+		this.leftCh = leftCh;
+	}
+	
+	public MyTreeNode(Ttype element) {
+		setElement(element);
+	}
+	
+	public String toString() {
+		return "" + element;
+	}
 }
